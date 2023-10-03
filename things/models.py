bde5c1e6f5b37c91e0951b93063338d06ad789dd
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-10-03 12:06:28 +0100

    Add validators to the quantity field

--- things/models.py
+++ things/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 from django.db.models import Model
+import django.core.validators 
 # Create your models here.
 
 class Thing(Model):
     name = models.CharField(
         verbose_name="name", 
-        unique=False, 
-        max_length=30)
+        unique=True, 
+        max_length=30,
+        blank=False)
     
     description = models.CharField(
         verbose_name='description', 
-        unique=False, 
-        max_length=120)
+        unique=False,
+        max_length=120,
+        blank=True)
     
     quantity = models.IntegerField(
-        verbose_name='quantity')
+        verbose_name='quantity',
+        unique=False,
+        validators=[
+            django.core.validators.MinValueValidator(0),
+            django.core.validators.MaxValueValidator(100),
+        ],
+        )
